a9335874bf566c70741270630d01d42682c2cbd5
fbusso <fbusso@frsf.utn.edu.ar>
2020-09-23 21:55:33 -0300

    Agregar funcionalidad a los botones
    Se crean los métodos para que los botones funcionen

--- app/src/main/java/isi/dam/sendmeal/HomeActivity.java
+++ app/src/main/java/isi/dam/sendmeal/HomeActivity.java
@@ -3,20 +3,51 @@ package isi.dam.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        myToolbar.inflateMenu(R.menu.menu);
-
-
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.registrar:
+                Log.d("onOptionsItemSelected", "registrar");
+                break;
+
+            case R.id.agregar_item:
+                Log.d("onOptionsItemSelected", "agregar_item");
+                break;
+
+            case R.id.lista_items:
+                Log.d("onOptionsItemSelected", "lista_items");
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
